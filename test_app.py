1f80ae3363df90cfa6cd6710dab9bc85299cd6b2
brijal08 <31299784+brijal08@users.noreply.github.com>
2024-07-26 00:22:15 -0400

    Add files via upload
    updated url verify test

--- test_app.py
+++ test_app.py
@@ -1,35 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import time
+from selenium.webdriver.support import expected_conditions as EC
 
-# Set up the WebDriver (Make sure you have the correct path to your chromedriver)
 options = webdriver.ChromeOptions()
+# options.add_argument("--headless")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# Open the Streamlit app
-driver.get('https://housepriceui-aocbtsgxyzsdudmixmzktu.streamlit.app/')  # Ensure your Streamlit app is running on this port
+driver.get("https://housepriceui-aocbtsgxyzsdudmixmzktu.streamlit.app/") 
 
-# Wait for the page to load
-time.sleep(3)
+# Wait for the page to load completely
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
-# Input data into the text fields
-driver.find_element(By.XPATH, "//input[@placeholder='income in block group:']").send_keys("8.3252")
-driver.find_element(By.XPATH, "//input[@placeholder='house age in block group:']").send_keys("41")
-driver.find_element(By.XPATH, "//input[@placeholder='number of rooms per household:']").send_keys("6.9841")
-driver.find_element(By.XPATH, "//input[@placeholder='number of bedrooms per household:']").send_keys("1.0238")
-driver.find_element(By.XPATH, "//input[@placeholder='population:']").send_keys("322")
-driver.find_element(By.XPATH, "//input[@placeholder='number of household members:']").send_keys("2.5556")
-driver.find_element(By.XPATH, "//input[@placeholder='Latitude:']").send_keys("37.88")
-driver.find_element(By.XPATH, "//input[@placeholder='Longitude:']").send_keys("-122.23")
+# Switch to the first <iframe> (index 0)
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(0))
 
-# Click the "Predict" button
-predict_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Predict')]")
-predict_button.click()
+# driver.switch_to.frame(0)
+input_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'text_input_1')))
+
+driver.find_element(By.ID, 'text_input_1').send_keys('8.3252')
+driver.find_element(By.ID, 'text_input_2').send_keys('41')
+driver.find_element(By.ID, 'text_input_3').send_keys('6.0')
+driver.find_element(By.ID, 'text_input_4').send_keys('1.0')
+driver.find_element(By.ID, 'text_input_5').send_keys('1.0')
+driver.find_element(By.ID, 'text_input_6').send_keys('2.5')
+driver.find_element(By.ID, 'text_input_7').send_keys('37.88')
+driver.find_element(By.ID, 'text_input_8').send_keys('-122.23')
+
+driver.find_element(By.XPATH, '//button/div/p').click()
+
 
 # Wait for the response
 try:
@@ -37,7 +38,7 @@ try:
         EC.text_to_be_present_in_element((By.TAG_NAME, 'body'), 'House Price is')
     )
     # Capture and print the result
-    result = driver.find_element(By.XPATH, "//div[contains(text(), 'House Price is')]").text
+    result = driver.find_element(By.XPATH, "//div/div/p").text
     print(result)
 except Exception as e:
     print("Error: Unable to get the prediction result.")
